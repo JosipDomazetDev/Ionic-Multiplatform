c5b59aebec76a459e39fa154061c92c51a55433b
Josip Domazet <josip.domazet.corp@gmail.com>
2023-09-25 10:50:39 +0200

    added splash screen after 3 hours of purgatory

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,25 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   server: {
     androidScheme: 'https'
-  }
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 100,
+      backgroundColor: "#ce7d03",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#ffffff",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
